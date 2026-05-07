0dc3f1de23988315108bf97b332f99ca771945cd
siddharthakkinepalli <siddharthakkinepalli@users.noreply.github.com>
2026-05-07 11:16:46 +0200

    Bump Android version to 1.0.15 (16) for Play release

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.jugaad.home"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.0.14"
+        versionCode = 16
+        versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
