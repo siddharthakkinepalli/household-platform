306a39b2e145e1e5327163cab5ba393ca9402e8c
siddharthakkinepalli <siddharthakkinepalli@users.noreply.github.com>
2026-05-07 12:09:30 +0200

    Bump Android version to 1.0.18 (19) for internal testing

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.jugaad.home"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.0.17"
+        versionCode = 19
+        versionName = "1.0.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
